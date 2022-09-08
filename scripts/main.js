4e3db754ccc2a0649cca7cb849b2df824fd3892a
mametaihoo <mametaihoo@gmail.com>
2022-09-08 22:29:27 +0300

    more edits, added POLO page

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 var dropdown = document.querySelector('.dropdown');
 var dropdowncontent = document.querySelector('.dropdown-content');
-var galka1 = document.querySelector('.galka1');
-var galka2 = document.querySelector('.galka2');
+var firstlink = document.querySelector('.first-link');
+//var galka1 = document.querySelector('.galka1');
+//var galka2 = document.querySelector('.galka2');
+
 dropdown.onclick = function(){
   dropdowncontent.classList.toggle('active');
-  galka1.classList.toggle('galka-active1');
-  galka2.classList.toggle('galka-active2');
-    console.log(checked)
+  firstlink.classList.toggle('galka-active');
+  //galka1.classList.toggle('galka-active1');
+  //galka2.classList.toggle('galka-active2');
+    console.log('Classes toggled')
 };
